f7cc7d0aa67ed150dd1d869926285704aaf47c44
Guilherme Henrik Guedes de Souza <72047224+henrikgui@users.noreply.github.com>
2021-11-04 19:36:35 -0300

    Delete api directory

--- api/src/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const URI = "mongodb://localhost/petfood";
-
-mongoose.set("debug", true);
-
-mongoose
-  .connect(URI)
-  .then(() => console.log("DB is up."))
-  .catch((err) => console.log(err));
